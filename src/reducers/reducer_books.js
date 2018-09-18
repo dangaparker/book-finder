525a4349dbe286e37e2a6fb40aef2733cf17577c
Dan Parker <danparker55@gmail.comgit config --global user.name Dan>
2018-09-18 16:36:36 -0700

    completed redux book list

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        {title: "JavaScript: The Good Parts", pages: 101},
+        {title: "Harry Potter", pages: 485},
+        {title: "The Dark Tower", pages:589},
+        {title: "Eloquent Ruby", pages: 39}
+    ] 
+}
